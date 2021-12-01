bd1d5563660399d35cd2a6feba5bcfbe439e4197
std4453 <me@std4453.com>
2021-12-02 02:26:27 +0800

    Rewrite jfdmk with new jellyfin-xposed

--- src/index.js
+++ src/index.js
@@ -1,63 +1,57 @@
 import { BehaviorSubject } from "rxjs";
 import commentsEffect from "./effect";
+import shimmer from "shimmer";
 
-let unmount = null;
 const baseURL = new URL(document.currentScript.src);
 const basePath = `${baseURL.protocol}//${baseURL.host}`;
 const itemIdSubject = new BehaviorSubject("");
 
+const preHook = (obj, name, hook) => {
+  shimmer.wrap(
+    obj,
+    name,
+    (original) =>
+      function () {
+        hook(...arguments);
+        return original.apply(this, arguments);
+      }
+  );
+};
+
 if (Math.max(window.innerWidth, window.innerHeight) >= 1000) {
-  const tryMount = () => {
-    if (location.hash !== "#!/video") return false;
-    const video = document.querySelector("video");
-    if (!video) return false;
-    const page = Array.from(
-      document.querySelectorAll("div[id=videoOsdPage]")
-    ).find((el) => !el.classList.contains("hide"));
-    if (!page) return false;
+  const videoPlayer = Xp.playback.playbackmanager.x.playbackManager
+    .getPlayers()
+    .find(({ id }) => id === "htmlvideoplayer");
+  shimmer.wrap(
+    videoPlayer,
+    "createMediaElement",
+    (original) =>
+      async function (options) {
+        itemIdSubject.next(options.item.Id);
+        return (videoPlayer.videoElement = 
+          await original.apply(this, arguments));
+      }
+  );
+  preHook(videoPlayer, "onStartedAndNavigatedToOsd", () => {
+    if (videoPlayer.unmount) {
+      return;
+    }
+    console.log("[jfdmk] initializing comments...");
+    const page = Xp.viewManager._.currentView();
     const buttons = page.querySelector(".buttons.focuscontainer-x");
-    if (!buttons) return false;
     const osdTimeText = buttons.querySelector(".osdTimeText");
-    if (!osdTimeText) return false;
-    if (!itemIdSubject.getValue()) return false;
-    if (!unmount) {
-      console.log("[jfdmk] initializing comments...");
-      unmount = commentsEffect({
-        video,
-        buttons,
-        osdTimeText,
-        itemIdSubject,
-        basePath,
-      });
-    }
-    return true;
-  };
-  const update = () => {
-    if (!tryMount()) {
-      if (unmount) {
-        console.log("[jfdmk] finalizing comments...");
-        unmount();
-        unmount = null;
-        itemIdSubject.next("");
-      }
-    }
-  };
-  setInterval(update, 250);
-
-  // intercept fetch to get itemId
-  const originalFetch = window.fetch;
-  window.fetch = (...args) => {
-    const url = args[0];
-    {
-      const result = /\/Items\/([0-9a-fA-F]+)\/PlaybackInfo/.exec(url);
-      if (result) {
-        if (itemIdSubject.getValue() !== result[1]) {
-          itemIdSubject.next(result[1]);
-        }
-      }
-    }
-    return originalFetch(...args);
-  };
-
-  console.log("[jfdmk] waiting...");
+    videoPlayer.unmount = commentsEffect({
+      video: videoPlayer.videoElement,
+      buttons,
+      osdTimeText,
+      itemIdSubject,
+      basePath,
+    });
+  });
+  preHook(videoPlayer, "destroy", () => {
+    console.log("[jfdmk] finalizing comments...");
+    videoPlayer.unmount?.();
+    videoPlayer.unmount = undefined;
+    itemIdSubject.next("");
+  });
 }
